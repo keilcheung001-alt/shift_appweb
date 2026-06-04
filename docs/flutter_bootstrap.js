b78e85890c2f4251d3c6977acee51d894b7b6b2a
keilcheung001-alt <keilcheung001-alt@users.noreply.github.com>
2026-06-04 12:59:36 +0800

    更新網頁版

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1520281180" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3218807419" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
