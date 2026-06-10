d8f82a80c533c2f43b193f44d7c7acd03ca329ca
keilcheung001-alt <keilcheung001@gmail.com>
2026-06-10 14:06:37 +0000

    Deploying to gh-pages from @ keilcheung001-alt/shift_appweb@3c26421f6af367b53031291c40cd663a55703bae 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3658427394" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3202295712" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
